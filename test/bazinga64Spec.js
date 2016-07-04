189fdc448d9f0ec4b601ce50aee714ad9d030ad5
Benny Neugebauer <bn@bennyn.de>
2016-07-04 12:35:56 +0200

    Added encoding test

--- test/bazinga64Spec.js
+++ test/bazinga64Spec.js
@@ -1,5 +1,23 @@
-describe("A suite", function () {
-  it("contains spec with an expectation", function () {
-    expect(true).toBe(true);
+describe('Base64', function () {
+
+  describe('Encoding', function () {
+
+    it('can encode a byte array', function () {
+      var array = [1, 2, 3, 4, 5, 6, 7, 8];
+      var arrayBuffer = new ArrayBuffer(array.length);
+      var arrayBufferView = new Uint8Array(arrayBuffer);
+
+      for (var i = 0; i < arrayBufferView.length; i++) {
+        arrayBufferView[i] = array[i];
+      }
+
+      var result = bazinga64.fromByteArray(arrayBufferView);
+      expect(result).toBe('AQIDBAUGBwg=');
+    });
+
+  });
+
+  describe('Decoding', function () {
+
   });
 });
