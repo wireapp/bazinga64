dd2c267f16f10cfbacfd2280610443483995d78d
Benny Neugebauer <bn@bennyn.de>
2016-07-07 13:44:01 +0200

    Refinement

--- dist/node/bazinga64.js
+++ dist/node/bazinga64.js
@@ -1,13 +1,20 @@
 "use strict";
 var base64 = require("base64-js");
+var DecodedData_1 = require("./DecodedData");
 var bazinga64;
 (function (bazinga64) {
+    function byteArrayToString(byteArray) {
+        return String.fromCharCode.apply(null, new Uint16Array(byteArray));
+    }
     function fromByteArray(decoded) {
         return base64.fromByteArray(decoded);
     }
     bazinga64.fromByteArray = fromByteArray;
     function toByteArray(encoded) {
-        return base64.toByteArray(encoded);
+        var decodedByteArray = base64.toByteArray(encoded);
+        var decodedString = byteArrayToString(decodedByteArray);
+        var decodedData = new DecodedData_1.default(decodedString, decodedByteArray);
+        return decodedData.decodedByteArray;
     }
     bazinga64.toByteArray = toByteArray;
 })(bazinga64 || (bazinga64 = {}));
