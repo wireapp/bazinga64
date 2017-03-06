83de83d954170b792150325d8ff9f2b9b3899444
Benny Neugebauer <bn@bennyn.de>
2017-03-06 12:39:21 +0100

    Added sodium compliance spec

---
+++ test/js/specs/ComplianceSpec.js
@@ -0,0 +1,28 @@
+describe('Compliance', function() {
+
+  describe('decode Base64', function() {
+
+    it('is compliant with libsodium on short sequences', function() {
+      var encoded = 'SGVsbG8=';
+
+      var decodedWithBazinga64 = bazinga64.Decoder.fromBase64(encoded).asBytes;
+      var decodedWithSodium = sodium.from_base64(encoded);
+
+      expect(decodedWithBazinga64).toEqual(decodedWithSodium);
+    });
+
+    it('is compliant with libsodium on long sequences', function() {
+      var encoded = 'pQABARn//wKhAFgg5fwzzahXsFp99ChcRC0/0qIr4vLCujkcRSOkziiTz8gDoQChAFggaK10DY60iH38gbXc9GoOrv+SqQ0p3AEsR0WjHQLkV5kE9g==';
+
+      var decodedWithBazinga64 = bazinga64.Decoder.fromBase64(encoded, false).asBytes;
+      var decodedWithSodium = sodium.from_base64(encoded);
+
+      console.log('A', decodedWithBazinga64);
+      console.log('B', decodedWithSodium);
+
+      expect(decodedWithBazinga64.length).toBe(decodedWithSodium.length);
+    });
+
+  });
+
+});
